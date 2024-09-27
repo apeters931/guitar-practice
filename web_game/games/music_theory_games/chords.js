d1b6c27ab67737e7add209cc3405f3b8f9b70713
Alec Peters <alecpeters@Alecs-Air.lan>
2024-09-26 23:48:28 -0500

    edit chords js

--- web_game/games/music_theory_games/chords.js
+++ web_game/games/music_theory_games/chords.js
@@ -1,40 +1,48 @@
 // to do:
 // make it so questions repeat
 // keep score (this should happen after we figure out how to make it repeat becuase they will relate to each other)
+//  - question number
+//  - number correct
+//  - percent correct
+//  - average answer time
+//  - score based on time and correctness
 // add difficulty selector
 // add it so syntax is less important in answer (how to input will be explained in the instructions section though but i'm not adding instructions till all other modules are done)
 
-let answer;
-let title = "Name the notes in ";
-let full_title;
-let correct_response = "Correct!";
-let incorrect_str = "Incorrect: ";
-let incorrect_response;
-let randIndex;
-let flag = true;
+var answer;
+var title = "Name the notes in ";
+var full_title;
+var correct_response = "Correct!";
+var incorrect_str = "Incorrect: ";
+var incorrect_response;
+var play_again_message = 'Press enter for next question';
+var randIndex;
+var flag = true;
 
-fetch("chords.json")
-.then(response => response.json())
-.then(data => {
-    console.log(data.length)
-    console.log(data)
-    while (flag) {
-        randIndex = Math.floor(Math.random() * data.length);
-        if (data[randIndex].Multiplier == "1") {
-            flag = false
+function askQuestion() {
+
+    fetch("chords.json")
+    .then(response => response.json())
+    .then(data => {
+        console.log(data.length)
+        console.log(data)
+        while (flag) {
+            randIndex = Math.floor(Math.random() * data.length);
+            if (data[randIndex].Multiplier == "1") {
+                flag = false
+            }
         }
-    }
-    console.log(randIndex);
-    chord = data[randIndex].Chords;
-    correct_answer = data[randIndex].Notes;
-    full_title = title.concat(chord);
-    document.getElementById("main_title").textContent = full_title;
-})
+        console.log(randIndex);
+        chord = data[randIndex].Chords;
+        correct_answer = data[randIndex].Notes;
+        full_title = title + chord;
+        document.getElementById("main_title").textContent = full_title;
+    })
+}
 
-
-document.getElementById("my_submit").onclick = function(){
+function clickFunction(){
     answer = document.getElementById("user_input").value
-    incorrect_response = incorrect_str.concat(correct_answer)
+    incorrect_response = incorrect_str + correct_answer
 
     if (answer == correct_answer) {
         document.getElementById("message").textContent = correct_response
@@ -43,4 +51,16 @@ document.getElementById("my_submit").onclick = function(){
     else {
         document.getElementById("message").textContent = incorrect_response
     }
+
+    document.getElementById("play_again").textContent = play_again_message
+
+    document.addEventListener('keydown', function(event) {
+        if (event.key === 'Enter') {
+            console.log("test");
+        }
+      });
+
 }
+
+window.onload = askQuestion;
+document.getElementById("my_submit").onclick = clickFunction;
