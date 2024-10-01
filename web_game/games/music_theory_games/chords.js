35d291d79714735ebce7b6561786667526575a07
Alec Peters <alecpeters@Alecs-Air.lan>
2024-10-01 08:44:22 -0500

    string functions and add score

--- web_game/games/music_theory_games/chords.js
+++ web_game/games/music_theory_games/chords.js
@@ -1,9 +1,9 @@
 // to do:
-// can only submit answer once -- right now you can click submit multiple times and it keeps counting the answer for that question
-// round percentage
-// average answer time
 // score based on time, correctness, and points multiplier
-// add it so syntax is less important in answer (how to input will be explained in the instructions section though but i'm not adding instructions till all other modules are done)
+// - x 2 if you're answer time is less than you're median time
+// move score down so it won't move when the correct/incorrect message appears
+// add instructions window when you first open game
+// move logic to it's own function to make game flow more obvious
 
 // for developing
 // python3 -m http.server
@@ -11,10 +11,15 @@
 
 var counter = 1;
 let correctAnswer = 0;
+var avgTimeList = [];
+var score = 0;
 
 function gameLoop() {
 
     var answer;
+    var answerCleaned;
+    var correct_answer;
+    var correctAnswerCleaned;
     var title = "Name the notes in ";
     var full_title;
     var correct_response = "Correct!";
@@ -31,59 +36,70 @@ function gameLoop() {
     var percent_str = "Percentage: ";
     var time_str = "Average time: ";
     var score_str = "Score: ";
+    var avgTime;
+    let time = 0;
+    var timerElement;
+    var countUp;
 
-    console.log(counter)
     fetch("chords.json")
     .then(response => response.json())
     .then(data => {
-        // console.log(data.length)
-        // console.log(data)
-        console.log(clickedFlag);
         while (flag) {
             difficulty = probabilityArray[Math.floor(Math.random()*probabilityArray.length)];
-            // console.log(difficulty)
             randIndex = Math.floor(Math.random() * data.length);
             if (data[randIndex].Multiplier == difficulty) {
                 flag = false
             }
         }
-        // console.log(randIndex);
         chord = data[randIndex].Chords;
         correct_answer = data[randIndex].Notes;
+        correctAnswerCleaned = standardize_string(correct_answer);
         full_title = title + chord;
         document.getElementById("main_title").textContent = full_title;
 
+        // timer
+        timerElement = document.getElementById("timer")
+        countUp = setInterval(() => {
+            time++;
+            timerElement.textContent = time;
+
+        },1000);
+
         // set scoreboard
         question_number = question_str + counter.toString();
-        document.getElementById("question_num").textContent = question_number;
         if (counter == 1) {
-            document.getElementById("correct_num").textContent = correct_str + "--";
-            document.getElementById("correct_perc").textContent = percent_str + "--";
+            document.getElementById("scoreboard").textContent = score_str + "--";;
+            document.getElementById("score_line_1").textContent = question_number + ", " + correct_str + "--" +  ", " + percent_str + "--" +  ", " + time_str + "--";
         }
-        // temp until this is calculated
-        document.getElementById("avg_time").textContent = time_str + "--";;
-        document.getElementById("scoreboard").textContent = score_str + "--";;
+        
     })
 
     document.getElementById("my_submit").onclick = function() {
         if (document.getElementById("user_input").value != '') {
-            // console.log("clicked");
-            // console.log(document.getElementById("user_input").value);
-            answer = document.getElementById("user_input").value
-            incorrect_response = incorrect_str + correct_answer
+            pausedTime = time;
+            avgTimeList.push(pausedTime);
+            avgTime = avg_array(avgTimeList);
+            clearInterval(countUp);
+            answer = document.getElementById("user_input").value;
+            answerCleaned = standardize_string(answer);
+            incorrect_response = incorrect_str + format_answer(correct_answer);
             clickedFlag = true;
 
-            if (answer == correct_answer) {
+            //console.log(ans)
+
+            if (answerCleaned == correctAnswerCleaned) {
                 document.getElementById("message").textContent = correct_response;
                 correctAnswer++;
+                score = score + (100 * parseInt(difficulty));
             }
 
             else {
                 document.getElementById("message").textContent = incorrect_response;
+                score = score - 100;
             }
 
             if (correctAnswer > 0) {
-                correctPercent = (correctAnswer/counter) * 100;
+                correctPercent = ((correctAnswer/counter) * 100).toFixed(2);
             }
 
             else {
@@ -91,12 +107,15 @@ function gameLoop() {
             }
 
             // update scoreboard
-            question_number = question_str + counter.toString();
-            correct_count = correct_str + correctAnswer.toString();
-            correct_percentage = percent_str + correctPercent.toString() + "%";
-            document.getElementById("question_num").textContent = question_number;
-            document.getElementById("correct_num").textContent = correct_count;
-            document.getElementById("correct_perc").textContent = correct_percentage;
+            if (counter >= correctAnswer) {
+                question_number = question_str + counter.toString();
+                correct_count = correct_str + correctAnswer.toString();
+                correct_percentage = percent_str + correctPercent.toString() + "%";
+                average_time_string = time_str + avgTime + "s";
+                total_score = score_str + score.toString();
+                document.getElementById("scoreboard").textContent = total_score;
+                document.getElementById("score_line_1").textContent = question_number + ", " + correct_count + ", " + correct_percentage + ", " + average_time_string;
+            }
 
         }
 
@@ -105,6 +124,7 @@ function gameLoop() {
     document.addEventListener('keydown', function(event) {
         if (event.key === 'Enter' && clickedFlag == true) {
             // reset variables
+            time = 0;
             clickedFlag = false;
             document.getElementById('user_input').value = '';
             document.getElementById('message').textContent = '';
@@ -113,6 +133,28 @@ function gameLoop() {
         }
     });
 
+}
+
+// find average for items in array
+function avg_array(arr) {
+    let sum = 0;
+    for (let i = 0; i < arr.length; i++) {
+        sum += arr[i];
+    }
+    return (sum / arr.length).toFixed(2);
+}
+
+// make string lowercase and comma seperated
+function standardize_string(str){
+    let lowerStr = str.toLowerCase();
+    let standardizedStr = lowerStr.replaceAll(" ",",")
+    return standardizedStr
+}
+
+// format answer string for screen
+function format_answer(str){
+    let answer  = str.replaceAll(",",", ")
+    return answer
 }
 
 if (counter == 1) {
