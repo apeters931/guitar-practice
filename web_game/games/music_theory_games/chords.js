577b6c66fb3a811e74e381130acaae82025185f1
Alec Peters <alecpeters@Alecs-Air.lan>
2024-10-02 10:03:53 -0500

    add comment

--- web_game/games/music_theory_games/chords.js
+++ web_game/games/music_theory_games/chords.js
@@ -1,21 +1,24 @@
 // to do:
+// score is wrong after first question
 // move score down so it won't move when the correct/incorrect message appears
 // add instructions window when you first open game
-// move logic to it's own function to make game flow more obvious
 
-// for developing
+// for developing:
 // python3 -m http.server
 // http://localhost:8000/guitar-practice/web_game/games/music_theory_games/chords.html
 
-// game variables - variable that will not be cleared after each question
+// game variables used for score
 var counter = 1;
-let correctAnswer = 0;
+var correctAnswer = 0;
+var correctPercent;
 var avgTimeList = [];
+var avgTime;
 var medianTimeList = [];
 var score = 0;
 
+// function for each question
 function gameLoop() {
-
+    // set variables
     var answer;
     var answerCleaned;
     var correct_answer;
@@ -30,13 +33,6 @@ function gameLoop() {
     var difficulty;
     var flag = true;
     var clickedFlag = false;
-    var correctPercent;
-    var question_str = "Question: ";
-    var correct_str = "Correct: ";
-    var percent_str = "Percentage: ";
-    var time_str = "Average time: ";
-    var score_str = "Score: ";
-    var avgTime;
     let time = 0;
     var timerElement;
     var countUp;
@@ -45,16 +41,24 @@ function gameLoop() {
     fetch("chords.json")
     .then(response => response.json())
     .then(data => {
+        // loop through until a chord is found that is the correct difficulty
         while (flag) {
+            // pick random item in probability array to be used as difficulty
             difficulty = probabilityArray[Math.floor(Math.random()*probabilityArray.length)];
+            // pick a random chord from th data
             randIndex = Math.floor(Math.random() * data.length);
+            // if the chord is the right difficulty stop looping
             if (data[randIndex].Multiplier == difficulty) {
                 flag = false
             }
         }
+        // get chord name from JSON
         chord = data[randIndex].Chords;
+        // get notes in chord from JSON
         correct_answer = data[randIndex].Notes;
+        // format string to be consistent for comparing
         correctAnswerCleaned = standardize_string(correct_answer);
+        // create title w/ chord name and add it to screen
         full_title = title + chord;
         document.getElementById("main_title").textContent = full_title;
 
@@ -67,78 +71,119 @@ function gameLoop() {
         },1000);
 
         // set scoreboard
-        question_number = question_str + counter.toString();
-        if (counter == 1) {
-            document.getElementById("scoreboard").textContent = score_str + "--";;
-            document.getElementById("score_line_1").textContent = question_number + ", " + correct_str + "--" +  ", " + percent_str + "--" +  ", " + time_str + "--";
-        }
+        generate_score(counter);
         
     })
 
+    // triggers when button gets clicked
     document.getElementById("my_submit").onclick = function() {
+        // need to have input for button clicked logic to run
         if (document.getElementById("user_input").value != '') {
+            // get the time the submit button was pressed
             pausedTime = time;
+            // add time to average time array
             avgTimeList.push(pausedTime);
+            // add time to median time array
             medianTimeList.push(pausedTime);
+            // calculate average and median times
             avgTime = avg_array(avgTimeList);
             medianTime = median(medianTimeList);
+            // stop the timer
             clearInterval(countUp);
+            // get user's answer
             answer = document.getElementById("user_input").value;
+            // convert to standard format for comparing
             answerCleaned = standardize_string(answer);
+            // create string for incorrect response by concatenating the correct answer
             incorrect_response = incorrect_str + format_answer(correct_answer);
+            // set flag to true that the button has been clicked
             clickedFlag = true;
-
-            //console.log(ans)
-
+            
+            // checks if input answer is equal to the correct answer from the data
             if (answerCleaned == correctAnswerCleaned) {
+                // add correct message to screen
                 document.getElementById("message").textContent = correct_response;
+                // adds one to the correct answer variable
                 correctAnswer++;
+                // adds 100 x the difficulty multiplier to the score variable
                 score = score + (100 * parseInt(difficulty));
+                // adds an addition 100 in the answer time was less than the median answer time
                 if (pausedTime < medianTime) {
                     score = score + 100;
                 }
             }
 
+            // when answer is not correct
             else {
+                // adds incorrect message to screen
                 document.getElementById("message").textContent = incorrect_response;
+                // subtracts 100 from score
                 score = score - 100;
             }
 
-            if (correctAnswer > 0) {
-                correctPercent = ((correctAnswer/counter) * 100).toFixed(2);
-            }
-
-            else {
-                correctPercent = 0;
-            }
+            // calculate correct percentage - MAKE FUNCTION
+            correctPercent = correct_percent(correctAnswer,counter);
 
             // update scoreboard
-            if (counter >= correctAnswer) {
-                question_number = question_str + counter.toString();
-                correct_count = correct_str + correctAnswer.toString();
-                correct_percentage = percent_str + correctPercent.toString() + "%";
-                average_time_string = time_str + avgTime + "s";
-                total_score = score_str + score.toString();
-                document.getElementById("scoreboard").textContent = total_score;
-                document.getElementById("score_line_1").textContent = question_number + ", " + correct_count + ", " + correct_percentage + ", " + average_time_string;
-            }
+            generate_score(counter,correctAnswer,correctPercent,avgTime,score)
 
         }
 
     }
 
+    // trigger when the enter key is clicked
     document.addEventListener('keydown', function(event) {
+        // only want enter key to do anything when the subit button was already clicked
         if (event.key === 'Enter' && clickedFlag == true) {
             // reset variables
             time = 0;
             clickedFlag = false;
             document.getElementById('user_input').value = '';
             document.getElementById('message').textContent = '';
+            // add one to the counter
             counter++;
+            // start from beging of game loop
             gameLoop(); 
         }
     });
 
+}
+
+// create scoreboard
+function generate_score(count,countCorrect=0,perecent=0,average=0,score=0){
+    // labels for scores
+    var question_str = "Question: ";
+    var correct_str = "Correct: ";
+    var percent_str = "Percentage: ";
+    var time_str = "Average time: ";
+    var score_str = "Score: ";
+
+    if (count == 1) {
+        question_number = question_str + count.toString();
+        document.getElementById("scoreboard").textContent = score_str + "--";;
+        document.getElementById("score_line_1").textContent = question_number + ", " + correct_str + "--" +  ", " + percent_str + "--" +  ", " + time_str + "--";
+    }
+
+    else if (counter >= correctAnswer) {
+        question_number = question_str + counter.toString();
+        correct_count = correct_str + correctAnswer.toString();
+        correct_percentage = percent_str + correctPercent.toString() + "%";
+        average_time_string = time_str + avgTime + "s";
+        total_score = score_str + score.toString();
+        document.getElementById("scoreboard").textContent = total_score;
+        document.getElementById("score_line_1").textContent = question_number + ", " + correct_count + ", " + correct_percentage + ", " + average_time_string;
+    }
+}
+
+// calculate correct percentage
+function correct_percent(countCorrect,count){
+    if (countCorrect > 0) {
+        return ((countCorrect/count) * 100).toFixed(2);
+    }
+
+    else {
+        return 0;
+    }
 }
 
 // find average for items in array
