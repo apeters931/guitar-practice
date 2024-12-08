1a8651f77c851d0f5603223c66ff79d54668e7d4
Alec Peters <alecpeters@Alecs-Air.lan>
2024-12-08 12:38:44 -0600

    re org

--- pygame_app/main.py
+++
@@ -1,9 +0,0 @@
-import sys
-import pygame
-import fretboard_exercises.guitar_fretboard_exercise as gfe
-# import scales_and_arpeggios
-# #import scales_and_arpeggios.game_logic as game_logic
-import ear_training_exercises.ear_training_main as et
-import general_package.general 
-
-gfe.main()
