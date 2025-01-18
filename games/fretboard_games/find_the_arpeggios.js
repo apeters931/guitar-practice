33dc9fcdee9fe1e7c5243b9934c678848784af89
Alec Peters <alecpeters@Alecs-MacBook-Air.local>
2025-01-18 16:36:35 -0600

    get random scales

--- games/fretboard_games/find_the_arpeggios.js
+++ games/fretboard_games/find_the_arpeggios.js
@@ -1,52 +1,81 @@
-    // initialize variables
-    let x;
-    let y;
-    const canvas = document.getElementById("myCanvas");
-    const ctx = canvas.getContext("2d");
-    canvas.width = 110;
-    canvas.height = 650;
-    var key = ['c','d','e','f','g','a','b']
-
-    // MAIN GAME LOGIC
-    // read JSON fretboard data
-    fetch("fretboard.json")
+    var randIndex;
+    var scale;
+    var note;
+    var scaleName;
+    var type;
+    var scaleArray;
+    var returnArray = [];
+    
+    fetch("scales.json")
     .then(response => response.json())
     .then(data => {
-        // save JSON data as fretboard
-        let fretboard = data;
-        // draw guitar neck on load
-        const img = new Image();
-        img.src = "images/guitar_neck_vertical.png";
-        img.addEventListener("load", function(){
-            ctx.drawImage(img, 0, 0, 110, 650)
-        })
-        // find location of clicks
-        canvas.addEventListener("click", function(event){
-            // gets x & y cordinates for click position
-            x = event.offsetX;
-            y = event.offsetY;
-            // gets cordinates for closest note
-            let final_xy = findClosestNote(x, y, fretboard)
-            // draws dot using the closest note cordinates
-            if (key.includes(final_xy[2])) {
-                var color = '#47ca7c'
-            }
-            else {
-                color = '#ff0000'
-            }
-            ctx.fillStyle = color;
-            ctx.beginPath();
-            ctx.arc(final_xy[0], final_xy[1], 4, 0, Math.PI*2);
-            ctx.fill();
-            ctx.stroke;
-            console.log(final_xy[2])
-        });
-        // clear dots when the backspacen key is pressed
-        document.addEventListener("keydown", function(event){
-            if (event.key == 'Backspace') {
-                ctx.clearRect(0, 0, 110, 650);
+        randIndex = Math.floor(Math.random() * data.length);
+        scale = data[randIndex].SCALE;
+        scaleArray = scale.split(',');
+        returnArray.push(scaleArray);
+        note = data[randIndex].NOTE;
+        returnArray.push(note);
+        scaleName = data[randIndex].NAME;
+        returnArray.push(scaleName);
+        type = data[randIndex].TYPE;
+        returnArray.push(type);
+
+        // initialize variables
+        let x;
+        let y;
+        const canvas = document.getElementById("myCanvas");
+        const ctx = canvas.getContext("2d");
+        canvas.width = 110;
+        canvas.height = 650;
+        var key = returnArray[0];
+        var keyNote = returnArray[1];
+        var keyType = returnArray[3];
+        var keyScaleName = returnArray[2];
+        var lowerKey = key.map(key => key.toLowerCase());
+        document.getElementById("main_title").textContent = keyNote + ' ' + keyType + ' ' + keyScaleName;
+        console.log(lowerKey);
+
+        // MAIN GAME LOGIC
+        // read JSON fretboard data
+        fetch("fretboard.json")
+        .then(response => response.json())
+        .then(data => {
+            // save JSON data as fretboard
+            let fretboard = data;
+            // draw guitar neck on load
+            const img = new Image();
+            img.src = "images/guitar_neck_vertical.png";
+            img.addEventListener("load", function(){
                 ctx.drawImage(img, 0, 0, 110, 650)
-            }
+            })
+            // find location of clicks
+            canvas.addEventListener("click", function(event){
+                // gets x & y cordinates for click position
+                x = event.offsetX;
+                y = event.offsetY;
+                // gets cordinates for closest note
+                let final_xy = findClosestNote(x, y, fretboard)
+                // draws dot using the closest note cordinates
+                if (lowerKey.includes(final_xy[2])) {
+                    var color = '#47ca7c'
+                }
+                else {
+                    color = '#ff0000'
+                }
+                ctx.fillStyle = color;
+                ctx.beginPath();
+                ctx.arc(final_xy[0], final_xy[1], 4, 0, Math.PI*2);
+                ctx.fill();
+                ctx.stroke;
+                console.log(final_xy[2])
+            });
+            // clear dots when the backspacen key is pressed
+            document.addEventListener("keydown", function(event){
+                if (event.key == 'Backspace') {
+                    ctx.clearRect(0, 0, 110, 650);
+                    ctx.drawImage(img, 0, 0, 110, 650)
+                }
+            });
         });
     });
 
