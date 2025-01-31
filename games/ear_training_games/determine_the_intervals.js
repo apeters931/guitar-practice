6c7162a50a336afac8355a9b55c9c85098b16571
Alec Peters <alecpeters@Alecs-Air.lan>
2025-01-30 19:01:01 -0600

    intervals

---
+++ games/ear_training_games/determine_the_intervals.js
@@ -0,0 +1,10 @@
+var root;
+var intervalNote;
+
+fetch("intervals.json")
+.then(response => response.json())
+.then(data => {
+    randIndex = Math.floor(Math.random() * data.length);
+    root = data[randIndex].root_id;
+    intervalNote = data[randIndex].third
+});
